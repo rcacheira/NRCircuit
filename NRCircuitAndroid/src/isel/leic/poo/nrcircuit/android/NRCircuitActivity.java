113a1206456d825414c8a60f4abd45c70ebcfca2
Ricardo Cacheira <rcacheira@gmail.com>
2014-05-26 12:06:43 +0100

    Projects Creation

---
+++ NRCircuitAndroid/src/isel/leic/poo/nrcircuit/android/NRCircuitActivity.java
@@ -0,0 +1,13 @@
+package isel.leic.poo.nrcircuit.android;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NRCircuitActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nrcircuit);
+	}
+
+}
